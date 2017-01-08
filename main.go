6f81b70a0901fb37e524de668b210cb2ed1f7264
Clifton Kaznocha <ckaznocha@gmail.com>
2017-01-08 13:28:32 -0800

    more spiffier main.go 🎩

--- main.go
+++ main.go
@@ -6,7 +6,6 @@ import (
 
 	"github.com/ckaznocha/protoc-gen-lint/linter"
 	"github.com/golang/protobuf/proto"
-	"github.com/golang/protobuf/protoc-gen-go/descriptor"
 	protoc "github.com/golang/protobuf/protoc-gen-go/plugin"
 )
 
@@ -19,21 +18,16 @@ func panicOnError(err error) {
 func main() {
 
 	var (
-		err              error
-		data             []byte
-		protoFiles       []*descriptor.FileDescriptorProto
 		totalErrors      int
-		generatorRequest = new(protoc.CodeGeneratorRequest)
+		generatorRequest protoc.CodeGeneratorRequest
 	)
 
-	data, err = ioutil.ReadAll(os.Stdin)
+	data, err := ioutil.ReadAll(os.Stdin)
 	panicOnError(err)
 
-	err = proto.Unmarshal(data, generatorRequest)
-	panicOnError(err)
+	panicOnError(proto.Unmarshal(data, &generatorRequest))
 
-	protoFiles = generatorRequest.GetProtoFile()
-	for _, file := range protoFiles {
+	for _, file := range generatorRequest.GetProtoFile() {
 		numErrors, err := linter.LintProtoFile(file, os.Stderr)
 		panicOnError(err)
 		totalErrors += numErrors
