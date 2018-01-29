c840e250919e436857c71cf8ed571183c4ee7153
Roman Volosatovs <rvolosatovs@riseup.net>
2018-01-29 23:31:57 +0100

    Add sort_imports parameter for import statement order check

--- main.go
+++ main.go
@@ -3,11 +3,16 @@ package main
 import (
 	"io/ioutil"
 	"os"
+	"strings"
 
 	"github.com/ckaznocha/protoc-gen-lint/linter"
 	"github.com/golang/protobuf/proto"
 	protoc "github.com/golang/protobuf/protoc-gen-go/plugin"
 )
+
+// SortImports represents the parameter, which can be specified to the tool invocation
+// to enable checking, whether the proto file imports are sorted alphabetically.
+const SortImports = "sort_imports"
 
 func panicOnError(err error) {
 	if err != nil {
@@ -16,11 +21,20 @@ func panicOnError(err error) {
 }
 
 func main() {
-
 	var (
 		totalErrors      int
 		generatorRequest protoc.CodeGeneratorRequest
+		parameters       struct {
+			SortImports bool
+		}
 	)
+
+	for _, p := range strings.Split(generatorRequest.GetParameter(), ",") {
+		switch p {
+		case SortImports:
+			parameters.SortImports = true
+		}
+	}
 
 	data, err := ioutil.ReadAll(os.Stdin)
 	panicOnError(err)
@@ -28,7 +42,11 @@ func main() {
 	panicOnError(proto.Unmarshal(data, &generatorRequest))
 
 	for _, file := range generatorRequest.GetProtoFile() {
-		numErrors, err := linter.LintProtoFile(file, os.Stderr)
+		numErrors, err := linter.LintProtoFile(linter.Config{
+			ProtoFile:   file,
+			OutFile:     os.Stderr,
+			SortImports: parameters.SortImports,
+		})
 		panicOnError(err)
 		totalErrors += numErrors
 	}
