ea7f0777bcbefdf037c82626d6d9f764502fefbb
Wyatt Anderson <wanderson@gmail.com>
2022-11-08 13:45:31 -0800

    Support 'optional' fields in proto3 (#34)

--- main.go
+++ main.go
@@ -1,63 +1,64 @@
 package main
 
 import (
+	"errors"
+	"flag"
 	"fmt"
-	"io"
 	"os"
-	"strings"
 
-	"google.golang.org/protobuf/proto"
+	"google.golang.org/protobuf/compiler/protogen"
 	"google.golang.org/protobuf/types/pluginpb"
 
 	"github.com/ckaznocha/protoc-gen-lint/linter"
 )
 
-// SortImports represents the parameter, which can be specified to the tool invocation
-// to enable checking, whether the proto file imports are sorted alphabetically.
-const SortImports = "sort_imports"
+type BoolFlag interface {
+	IsBoolFlag() bool
+}
+
+var errLint = errors.New("encountered lint errors")
 
 func main() {
-	var (
-		totalErrors      int
-		generatorRequest pluginpb.CodeGeneratorRequest
-		parameters       struct {
-			SortImports bool
-		}
-	)
+	var flags flag.FlagSet
+	sortImports := flags.Bool("sort_imports", false, "check whether or not the proto "+
+		"file imports are sorted alphabetically")
 
-	data, err := io.ReadAll(os.Stdin)
-	if err != nil {
-		panic(err)
-	}
+	protogen.Options{
+		ParamFunc: func(param, value string) error {
+			// For backwards compatibility, treat a present flag with an empty
+			// string value as boolean true. The Go flag module parsing
+			// handles this, but the protogen flag parse behavior doesn't,
+			// because it doesn't parse flags the same way.
+			f := flags.Lookup(param)
+			if f != nil {
+				if _, ok := f.Value.(BoolFlag); ok {
+					value = "true"
+				}
+			}
 
-	if err := proto.Unmarshal(data, &generatorRequest); err != nil {
-		panic(err)
-	}
+			return flags.Set(param, value)
+		},
+	}.Run(func(gen *protogen.Plugin) error {
+		gen.SupportedFeatures = uint64(pluginpb.CodeGeneratorResponse_FEATURE_PROTO3_OPTIONAL)
 
-	for _, p := range strings.Split(generatorRequest.GetParameter(), ",") {
-		switch strings.TrimSpace(p) {
-		case "":
-			continue
-		case SortImports:
-			parameters.SortImports = true
-		default:
-			fmt.Fprintf(os.Stderr, "Unmatched parameter: %s", p)
-			os.Exit(1)
-		}
-	}
+		totalErrors := 0
+		for _, f := range gen.Files {
+			numErrors, err := linter.LintProtoFile(linter.Config{
+				ProtoFile:   f.Proto,
+				OutFile:     os.Stderr,
+				SortImports: *sortImports,
+			})
+			if err != nil {
+				return err
+			}
 
-	for _, file := range generatorRequest.GetProtoFile() {
-		numErrors, err := linter.LintProtoFile(linter.Config{
-			ProtoFile:   file,
-			OutFile:     os.Stderr,
-			SortImports: parameters.SortImports,
-		})
-		if err != nil {
-			panic(err)
+			totalErrors += numErrors
 		}
 
-		totalErrors += numErrors
-	}
+		if totalErrors > 0 {
+			return fmt.Errorf("%w: %d total", errLint, totalErrors)
+		}
 
-	os.Exit(totalErrors)
+		return nil
+	})
 }
